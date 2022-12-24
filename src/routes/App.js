5e3359c71eaa15753f76ef78689c844c0ca828a3
franco7596 <franco_7_5_96@hotmail.com>
2022-12-24 19:06:54 -0300

    starting navegate bar

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,5 +1,15 @@
 import React from "react";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import Home from "../components/home/Home";
+import NavBar from "../components/navBar/NavBar";
 
 export default function App() {
-  return <div>indexApp</div>;
+  return (
+    <HashRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </HashRouter>
+  );
 }
