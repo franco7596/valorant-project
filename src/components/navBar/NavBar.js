89cb8da409de50fa7239dd52419316af7948fb80
franco7596 <franco_7_5_96@hotmail.com>
2023-01-07 13:24:33 -0300

    changes of the navegation bar

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,16 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import Brightness4Icon from "@mui/icons-material/Brightness4";
-import SwipeableDrawer from "@mui/material/SwipeableDrawer";
 import { createTheme, ThemeProvider } from "@mui/material";
+import "./navBar.css";
 
 const darkTheme = createTheme({
   palette: {
@@ -24,50 +23,39 @@ const darkTheme = createTheme({
 
 export default function NavBar() {
   return (
-    <Box
-      style={{
-        minHeight: "100%",
-        minWidth: "100%",
-        position: "fixed",
-        top: 0,
-        left: 0,
-      }}
-    >
-      <ThemeProvider theme={darkTheme}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              <SwipeableDrawer
-                anchor={anchor}
-                open={state[anchor]}
-                onClose={toggleDrawer(anchor, false)}
-                onOpen={toggleDrawer(anchor, true)}
-              >
-                {list(anchor)}
-              </SwipeableDrawer>
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              News
-            </Typography>
-            <Button color="inherit">
-              <Brightness4Icon />
-            </Button>
-            <Button color="inherit">
-              <LinkedInIcon />
-            </Button>
-            <Button color="inherit">
-              <GitHubIcon />
-            </Button>
-          </Toolbar>
-        </AppBar>
-      </ThemeProvider>
-    </Box>
+    <div className="nav_bar">
+      <Box
+        style={{
+          minHeight: "100%",
+          minWidth: "100%",
+          position: "fixed",
+          top: 0,
+          left: 0,
+        }}
+      >
+        <ThemeProvider theme={darkTheme}>
+          <AppBar position="static">
+            <div className="nav_bar-tool_bar">
+              <img
+                src="https://placeimg.com/80/40/arch"
+                className="nav_bar-img"
+              />
+              <h1 className="nav_bar-h1">Valorant Game</h1>
+              <div className="nav_bar-buttons">
+                <Button color="inherit">
+                  <Brightness4Icon />
+                </Button>
+                <Button color="inherit">
+                  <LinkedInIcon />
+                </Button>
+                <Button color="inherit">
+                  <GitHubIcon />
+                </Button>
+              </div>
+            </div>
+          </AppBar>
+        </ThemeProvider>
+      </Box>
+    </div>
   );
 }
