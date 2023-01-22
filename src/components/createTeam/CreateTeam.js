2d6e9e37dae0d9a364f2434fca904d5cdf95168b
franco7596 <franco_7_5_96@hotmail.com>
2023-01-22 12:45:38 -0300

    fix problems of resize

--- src/components/createTeam/CreateTeam.js
+++ src/components/createTeam/CreateTeam.js
@@ -12,119 +12,122 @@ import CardStatistics from "./CardStatistics";
 import "./createTeam.css";
 
 const initialstatistics = {
-	LURKER: 0,
-	LURKERStatus: false,
-	AWP: 0,
-	AWPStatus: false,
-	ENTRY: 0,
-	ENTRYStatus: false,
-	STRONHOLDERS: 0,
-	STRONHOLDERSStatus: false,
-	SMOKEADORES: 0,
-	SMOKEADORESStatus: false,
-	SPOTEADORES: 0,
-	SPOTEADORESStatus: false,
-	HEALERS: 0,
-	HEALERSStatus: false,
-	FLASHEADORES: 0,
-	FLASHEADORESStatus: false,
+  LURKER: 0,
+  LURKERStatus: false,
+  AWP: 0,
+  AWPStatus: false,
+  ENTRY: 0,
+  ENTRYStatus: false,
+  STRONHOLDERS: 0,
+  STRONHOLDERSStatus: false,
+  SMOKEADORES: 0,
+  SMOKEADORESStatus: false,
+  SPOTEADORES: 0,
+  SPOTEADORESStatus: false,
+  HEALERS: 0,
+  HEALERSStatus: false,
+  FLASHEADORES: 0,
+  FLASHEADORESStatus: false,
 };
 
 export default function CreateTeam() {
-	const maps = useSelector((state) => state.maps.maps);
-	const agents = useSelector((state) => state.agents.agents);
-	const dispach = useDispatch();
-	useEffect(() => {
-		if (!maps) dispach(startGetMaps());
-		if (!agents) dispach(startGetAgents());
-	}, []);
-	const {
-		statisticsTeam,
-		handleSelectMap,
-		handleChangeSelectedAgent,
-		optionsAutoComplete,
-		statusTeam,
-		selectedMap,
-		slectedAgent,
-		handleAgentToChange,
-	} = useCreateTeam(initialstatistics);
+  const maps = useSelector((state) => state.maps.maps);
+  const agents = useSelector((state) => state.agents.agents);
+  const dispach = useDispatch();
+  useEffect(() => {
+    if (!maps) dispach(startGetMaps());
+    if (!agents) dispach(startGetAgents());
+  }, []);
+  const {
+    statisticsTeam,
+    handleSelectMap,
+    handleChangeSelectedAgent,
+    optionsAutoComplete,
+    statusTeam,
+    selectedMap,
+    slectedAgent,
+    handleAgentToChange,
+  } = useCreateTeam(initialstatistics);
 
-	return maps && agents && selectedMap ? (
-		<div className="create_team-container">
-			<div className="create_team-container-maps">
-				<div>
-					<h4 className="create_team-h4">SELECT THE MATCH MAP</h4>
-					<p className="create_team-p">
-						It is important choose a map in order to now the needs that the team
-						have to solve to exploit the mayor probabilities that the map bring
-						us so much for the atack how to defend the importance sites.
-					</p>
-				</div>
-				<div className="create_team-container-maps-images">
-					<Carousel
-						elements={maps}
-						image="splash"
-						handleInformation={handleSelectMap}
-						showNavBar={false}
-						showArrows={true}
-						colorArrows="var(--black-color)"
-					/>
-					<div className="create_team-map_Info">
-						<h5 className="create_team-h5">{selectedMap.displayName}</h5>
-						<img
-							src={selectedMap.displayIcon}
-							className="create_team-container-maps-img"
-						/>
-					</div>
-				</div>
-				<h4 className="create_team-h4">
-					STATISTICS OF {selectedMap.displayName}
-				</h4>
-				<CardStatistics objectSelected={selectedMap} />
-			</div>
-			<div className="create_team-container-agents">
-				<h4 className="create_team-h4">SELECT YOUR TEAM</h4>
-				<p className="create_team-p">
-					Hear it is important creat your ideal team for the match map selected,
-					you have to be carfull with the needs of the map.
-				</p>
-				<TeamImages
-					agents={slectedAgent}
-					changeMidleAgent={handleAgentToChange}
-				/>
-				<div className="create_team-container-sutocomplete">
-					<h4>SELECT AN AGENT: </h4>
-					<Autocomplete
-						disablePortal
-						options={optionsAutoComplete}
-						onChange={(event, value) => handleChangeSelectedAgent(value)}
-						sx={{
-							width: "300px",
-							margin: "0 50px 0 50px",
-							backgroundColor: "var(--white-color)",
-						}}
-						renderInput={(params) => <TextField {...params} label="Agents" />}
-					/>
-				</div>
-				<h4 className="create_team-h4 statistics-team-title">
-					STATISTICS OF YOUR TEAM:
-				</h4>
-				{statusTeam && (
-					<div className="card card-body statistics-surpassed-team-card">
-						<h4 className="create_team-h4 statistics-surpassed-team-title">
-							You have a good team for this map
-						</h4>
-					</div>
-				)}
-				<CardStatistics objectSelected={statisticsTeam} />
-				<div className="cards-agents-container">
-					{slectedAgent.map((agent) => (
-						<CardAgent agent={agent} key={agent.uuid} />
-					))}
-				</div>
-			</div>
-		</div>
-	) : (
-		<Loading />
-	);
+  return maps && agents && selectedMap ? (
+    <div className="create_team-container">
+      <div className="create_team-container-maps">
+        <div>
+          <h4 className="create_team-h4">SELECT THE MATCH MAP</h4>
+          <p className="create_team-p">
+            It is important choose a map in order to now the needs that the team
+            have to solve to exploit the mayor probabilities that the map bring
+            us so much for the atack how to defend the importance sites.
+          </p>
+        </div>
+        <div className="create_team-container-maps-images">
+          <Carousel
+            elements={maps}
+            image="splash"
+            handleInformation={handleSelectMap}
+            showNavBar={false}
+            showArrows={true}
+            colorArrows="var(--black-color)"
+          />
+          <div className="create_team-map_Info">
+            <h5 className="create_team-h5">{selectedMap.displayName}</h5>
+            <img
+              src={selectedMap.displayIcon}
+              className="create_team-container-maps-img"
+            />
+          </div>
+        </div>
+        <h4 className="create_team-h4">
+          STATISTICS OF {selectedMap.displayName}
+        </h4>
+        <CardStatistics objectSelected={selectedMap} />
+      </div>
+      <div className="create_team-container-agents">
+        <h4 className="create_team-h4">SELECT YOUR TEAM</h4>
+        <p className="create_team-p">
+          Hear it is important creat your ideal team for the match map selected,
+          you have to be carfull with the needs of the map.
+        </p>
+        <TeamImages
+          agents={slectedAgent}
+          changeMidleAgent={handleAgentToChange}
+        />
+        <div className="create_team-container-sutocomplete">
+          <h4 className="create_team-h4 title-select-agent">
+            SELECT AN AGENT:{" "}
+          </h4>
+          <Autocomplete
+            disablePortal
+            options={optionsAutoComplete}
+            onChange={(event, value) => handleChangeSelectedAgent(value)}
+            sx={{
+              minWidth: "250px",
+              maxWidth: "300px",
+              margin: "0 50px 0 50px",
+              backgroundColor: "var(--white-color)",
+            }}
+            renderInput={(params) => <TextField {...params} label="Agents" />}
+          />
+        </div>
+        <h4 className="create_team-h4 statistics-team-title">
+          STATISTICS OF YOUR TEAM:
+        </h4>
+        {statusTeam && (
+          <div className="card card-body statistics-surpassed-team-card">
+            <h4 className="create_team-h4 statistics-surpassed-team-title">
+              You have a good team for this map
+            </h4>
+          </div>
+        )}
+        <CardStatistics objectSelected={statisticsTeam} />
+        <div className="cards-agents-container">
+          {slectedAgent.map((agent) => (
+            <CardAgent agent={agent} key={agent.uuid} />
+          ))}
+        </div>
+      </div>
+    </div>
+  ) : (
+    <Loading />
+  );
 }
--- src/components/teamImages/TeamImages.js
+++ src/components/teamImages/TeamImages.js
@@ -1,170 +1,150 @@
 import React, { useState } from "react";
 import "./teamImages.css";
 export default function TeamImages({ agents, changeMidleAgent }) {
-	const [moving, setMoving] = useState(false);
-	const [mouseLastPosition, setMouseLastPosition] = useState(0);
-	const [transform, setTransform] = useState(0);
-	const [position, setPosition] = useState(0);
-	const [imagesProperties, setImagesProperties] = useState([
-		{
-			order: 1,
-			scale: 1,
-			id: 4,
-		},
-		{
-			order: 2,
-			scale: 1.5,
-			id: 3,
-		},
-		{
-			order: 3,
-			scale: 2,
-			id: 2,
-		},
-		{
-			order: 4,
-			scale: 1.5,
-			id: 1,
-		},
-		{
-			order: 5,
-			scale: 1,
-			id: 0,
-		},
-	]);
+  const [moving, setMoving] = useState(false);
+  const [mouseLastPosition, setMouseLastPosition] = useState(0);
+  const [transform, setTransform] = useState(0);
+  const [position, setPosition] = useState(0);
+  const [imagesProperties, setImagesProperties] = useState([
+    {
+      order: 1,
+      scale: 1,
+      id: 4,
+    },
+    {
+      order: 2,
+      scale: 1.5,
+      id: 3,
+    },
+    {
+      order: 3,
+      scale: 2,
+      id: 2,
+    },
+    {
+      order: 4,
+      scale: 1.5,
+      id: 1,
+    },
+    {
+      order: 5,
+      scale: 1,
+      id: 0,
+    },
+  ]);
 
-	const handleMouseDown = (e) => {
-		setMoving(true);
-		setMouseLastPosition(e.pageX);
-		const transformResult =
-			window
-				.getComputedStyle(document.querySelector("#imageAgent1"))
-				.getPropertyValue("transform") === "none"
-				? 0
-				: window
-						.getComputedStyle(document.querySelector("#imageAgent1"))
-						.getPropertyValue("transform")
-						.split(",")[4]
-						.trim();
-		setTransform(parseInt(transformResult));
-	};
-	const handleMouseMove = (e) => {
-		const widthImage =
-			document.querySelector("#imageAgent1").clientWidth -
-			document.querySelector("#imageAgent1").clientWidth * 0.5;
-		if (moving) {
-			const difX = e.pageX - mouseLastPosition + transform;
-			if (difX >= widthImage) {
-				setMouseLastPosition(e.pageX);
-				setPosition(0);
-				handleChange();
-				return;
-			}
-			if (difX <= -widthImage) {
-				setMouseLastPosition(e.pageX);
-				setPosition(0);
-				handleChange(false);
-				return;
-			}
-			setPosition(difX);
-		}
-	};
+  const handleMouseDown = (e) => {
+    setMoving(true);
+    setMouseLastPosition(e.pageX);
+    const transformResult =
+      window
+        .getComputedStyle(document.querySelector("#imageAgent1"))
+        .getPropertyValue("transform") === "none"
+        ? 0
+        : window
+            .getComputedStyle(document.querySelector("#imageAgent1"))
+            .getPropertyValue("transform")
+            .split(",")[4]
+            .trim();
+    setTransform(parseInt(transformResult));
+  };
+  const handleMouseMove = (e) => {
+    const widthImage =
+      document.querySelector("#imageAgent1").clientWidth -
+      document.querySelector("#imageAgent1").clientWidth * 0.5;
+    if (moving) {
+      const difX = e.pageX - mouseLastPosition + transform;
+      if (difX >= widthImage) {
+        setMouseLastPosition(e.pageX);
+        setPosition(0);
+        handleChange();
+        return;
+      }
+      if (difX <= -widthImage) {
+        setMouseLastPosition(e.pageX);
+        setPosition(0);
+        handleChange(false);
+        return;
+      }
+      setPosition(difX);
+    }
+  };
 
-	const handleMouseUp = () => {
-		setMoving(false);
-		setPosition(0);
-		if (position >= 200) {
-			handleChange();
-		}
-		if (position < -200) {
-			handleChange(false);
-		}
-	};
-	const handleChange = (rigth = true) => {
-		const lastValue = imagesProperties.slice(-1);
-		const firstValue = imagesProperties.slice(0, 1);
-		const midleValue = imagesProperties.slice(1, imagesProperties.length - 1);
-		if (rigth) {
-			if (changeMidleAgent) changeMidleAgent(imagesProperties[3].id);
-			setImagesProperties([...midleValue, ...lastValue, ...firstValue]);
-		} else {
-			if (changeMidleAgent) changeMidleAgent(imagesProperties[1].id);
-			setImagesProperties([...lastValue, ...firstValue, ...midleValue]);
-		}
-	};
-	return (
-		<div
-			className="team_Images-container"
-			onMouseUp={handleMouseUp}
-			onMouseLeave={handleMouseUp}
-			onMouseMove={handleMouseMove}
-		>
-			<div
-				id="imageAgent1"
-				style={{
-					backgroundImage: `url(${agents[0].bustPortrait})`,
-					backgroundRepeat: "no-repeat",
-					backgroundSize: "contain",
-					backgroundPosition: "center",
-					position: "relative",
-					transform: `translateX(${position}px) scale(${imagesProperties[0].scale})`,
-					order: imagesProperties[0].order,
-				}}
-				className="team_Images-img"
-				onMouseDown={handleMouseDown}
-			/>
-			<div
-				style={{
-					backgroundImage: `url(${agents[1].bustPortrait})`,
-					backgroundRepeat: "no-repeat",
-					backgroundSize: "contain",
-					backgroundPosition: "center",
-					position: "relative",
-					transform: `translateX(${position}px) scale(${imagesProperties[1].scale})`,
-					order: imagesProperties[1].order,
-				}}
-				onMouseDown={handleMouseDown}
-				className="team_Images-img"
-			/>
-			<div
-				style={{
-					backgroundImage: `url(${agents[2].bustPortrait})`,
-					backgroundRepeat: "no-repeat",
-					backgroundSize: "contain",
-					backgroundPosition: "center",
-					position: "relative",
-					transform: `translateX(${position}px) scale(${imagesProperties[2].scale})`,
-					order: imagesProperties[2].order,
-				}}
-				onMouseDown={handleMouseDown}
-				className="team_Images-img"
-			/>
-			<div
-				style={{
-					backgroundImage: `url(${agents[3].bustPortrait})`,
-					backgroundRepeat: "no-repeat",
-					backgroundSize: "contain",
-					backgroundPosition: "center",
-					position: "relative",
-					transform: `translateX(${position}px) scale(${imagesProperties[3].scale})`,
-					order: imagesProperties[3].order,
-				}}
-				onMouseDown={handleMouseDown}
-				className="team_Images-img"
-			/>
-			<div
-				style={{
-					backgroundImage: `url(${agents[4].bustPortrait})`,
-					backgroundRepeat: "no-repeat",
-					backgroundSize: "contain",
-					backgroundPosition: "center",
-					position: "relative",
-					transform: `translateX(${position}px) scale(${imagesProperties[4].scale})`,
-					order: imagesProperties[4].order,
-				}}
-				className="team_Images-img"
-				onMouseDown={handleMouseDown}
-			/>
-		</div>
-	);
+  const handleMouseUp = () => {
+    setMoving(false);
+    setPosition(0);
+    if (position >= 200) {
+      handleChange();
+    }
+    if (position < -200) {
+      handleChange(false);
+    }
+  };
+  const handleChange = (rigth = true) => {
+    const lastValue = imagesProperties.slice(-1);
+    const firstValue = imagesProperties.slice(0, 1);
+    const midleValue = imagesProperties.slice(1, imagesProperties.length - 1);
+    if (rigth) {
+      if (changeMidleAgent) changeMidleAgent(imagesProperties[3].id);
+      setImagesProperties([...midleValue, ...lastValue, ...firstValue]);
+    } else {
+      if (changeMidleAgent) changeMidleAgent(imagesProperties[1].id);
+      setImagesProperties([...lastValue, ...firstValue, ...midleValue]);
+    }
+  };
+  return (
+    <div
+      className="team_images-container"
+      onMouseUp={handleMouseUp}
+      onMouseLeave={handleMouseUp}
+      onMouseMove={handleMouseMove}
+    >
+      <div
+        id="imageAgent1"
+        style={{
+          backgroundImage: `url(${agents[0].bustPortrait})`,
+          transform: `translateX(${position}px) scale(${imagesProperties[0].scale})`,
+          order: imagesProperties[0].order,
+        }}
+        className="team_Images-img"
+        onMouseDown={handleMouseDown}
+      />
+      <div
+        style={{
+          backgroundImage: `url(${agents[1].bustPortrait})`,
+          transform: `translateX(${position}px) scale(${imagesProperties[1].scale})`,
+          order: imagesProperties[1].order,
+        }}
+        onMouseDown={handleMouseDown}
+        className="team_Images-img"
+      />
+      <div
+        style={{
+          backgroundImage: `url(${agents[2].bustPortrait})`,
+          transform: `translateX(${position}px) scale(${imagesProperties[2].scale})`,
+          order: imagesProperties[2].order,
+        }}
+        onMouseDown={handleMouseDown}
+        className="team_Images-img"
+      />
+      <div
+        style={{
+          backgroundImage: `url(${agents[3].bustPortrait})`,
+          transform: `translateX(${position}px) scale(${imagesProperties[3].scale})`,
+          order: imagesProperties[3].order,
+        }}
+        onMouseDown={handleMouseDown}
+        className="team_Images-img"
+      />
+      <div
+        style={{
+          backgroundImage: `url(${agents[4].bustPortrait})`,
+          transform: `translateX(${position}px) scale(${imagesProperties[4].scale})`,
+          order: imagesProperties[4].order,
+        }}
+        className="team_Images-img"
+        onMouseDown={handleMouseDown}
+      />
+    </div>
+  );
 }
