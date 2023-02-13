78e03aa8e472fb4d8afbdca08c0e8012a92f6a93
franco7596 <franco_7_5_96@hotmail.com>
2023-02-13 18:01:50 -0300

    github link into navBar

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -11,56 +11,62 @@ import { useNavigate } from "react-router-dom";
 import homeIcon from "../../assets/icons/valorant-logo.jpg";
 
 const darkTheme = createTheme({
-	palette: {
-		mode: "dark",
-		primary: {
-			main: "#27272fff",
-		},
-	},
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#27272fff",
+    },
+  },
 });
 
 export default function NavBar() {
-	const navegation = useNavigate();
-	return (
-		<div className="nav_bar">
-			<Box
-				style={{
-					minWidth: "100%",
-					position: "fixed",
-					top: 0,
-					left: 0,
-					zIndex: 100,
-				}}
-			>
-				<ThemeProvider theme={darkTheme}>
-					<AppBar position="static">
-						<div className="nav_bar-tool_bar">
-							<img
-								src={homeIcon}
-								type="button"
-								className="nav_bar-img"
-								alt="#"
-								onClick={() => navegation("/")}
-							/>
-							<h1 className="nav_bar-h1">Valorant Game</h1>
-							<div className="nav_bar-buttons">
-								<Button color="inherit">
-									<a
-										href="https://ar.linkedin.com/in/franco-ribotta-274a211b0?trk=people-guest_people_search-card"
-										target="_blank"
-										className="nav_bar-a"
-									>
-										<LinkedInIcon />
-									</a>
-								</Button>
-								<Button color="inherit">
-									<GitHubIcon />
-								</Button>
-							</div>
-						</div>
-					</AppBar>
-				</ThemeProvider>
-			</Box>
-		</div>
-	);
+  const navegation = useNavigate();
+  return (
+    <div className="nav_bar">
+      <Box
+        style={{
+          minWidth: "100%",
+          position: "fixed",
+          top: 0,
+          left: 0,
+          zIndex: 100,
+        }}
+      >
+        <ThemeProvider theme={darkTheme}>
+          <AppBar position="static">
+            <div className="nav_bar-tool_bar">
+              <img
+                src={homeIcon}
+                type="button"
+                className="nav_bar-img"
+                alt="#"
+                onClick={() => navegation("/")}
+              />
+              <h1 className="nav_bar-h1">Valorant Game</h1>
+              <div className="nav_bar-buttons">
+                <Button color="inherit">
+                  <a
+                    href="https://ar.linkedin.com/in/franco-ribotta-274a211b0?trk=people-guest_people_search-card"
+                    target="_blank"
+                    className="nav_bar-a"
+                  >
+                    <LinkedInIcon />
+                  </a>
+                </Button>
+                <Button color="inherit">
+                  <a
+                    href="https://github.com/franco7596/valorant-project"
+                    target="_blank"
+                    className="nav_bar-a"
+                  >
+                    <GitHubIcon />
+                  </a>
+                </Button>
+              </div>
+            </div>
+          </AppBar>
+        </ThemeProvider>
+      </Box>
+    </div>
+  );
 }
